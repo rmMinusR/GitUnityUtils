0d84d41ab28d78e8a88a42ed4b8757083997c638
Robert Christensen <robert@rmMinusR.com>
2024-04-15 17:39:30 -0400

    Encapsulated unity file reading

--- split-asset.py
+++ split-asset.py
@@ -1,34 +1,33 @@
+import argparse
+parser = argparse.ArgumentParser(
+    prog="Split asset (Version Control Helpers for Unity)",
+    description="Splits a Unity file into multiple files, one for each object. Useful for complex merges/diffs."
+)
+parser.add_argument("input")
+parser.add_argument("output", nargs="?")
+
+
 import time
-import sys, os
+import sys, os, shutil
 from unityfile import *
 
 start = time.perf_counter()
 
-sourceFilePath = sys.argv[1]
-objects:dict[localid_t, UnityObject] = dict()
-fileHeader:str = None
+args = parser.parse_args()
+assetFile = UnityFile.fromPath(args.input)
 
-# Read data blocks
-with open(sourceFilePath, "r") as sourceFile:
-    lines = sourceFile.readlines()
-    lines = [(i[:-1] if i.endswith("\n") else i) for i in lines] # Remove trailing newlines
-
-    # Split into objects
-    indices = [i for i in range(len(lines)) if lines[i].startswith("--- !u!")] + [len(lines)]
-    fileHeader = "\n".join(lines[0:indices[0]])
-    for i in range(len(indices)-1):
-        objContent = "\n".join(lines[ indices[i] : indices[i+1] ])
-        obj = UnityObject.parse(objContent, lines)
-        objects[obj.localid] = obj
-    
-assert len(objects) > 0, "Nothing to split"
+# Setup destination
+if args.output == None: args.output = args.input+".split"
+if os.path.exists(args.output):
+    assert os.path.isdir(args.output), f"Expected a directory, but {parser.output} was a file"
+    shutil.rmtree(args.output)
+os.mkdir(args.output)
 
 # Write split data
-for k in objects.keys():
-    splitDir = sourceFilePath+".split/"
-    if not os.path.exists(splitDir): os.mkdir(splitDir)
-    with open(splitDir+str(k)+".yml", "w+") as objectFile:
-        objectFile.write(fileHeader+"\n"+str(objects[k])+"\n")
+for k in assetFile.objects.keys():
+    objFilePath = os.path.join(args.output, str(k)+".yml")
+    with open(objFilePath, "w+") as objectFile:
+        objectFile.write(assetFile.header+"\n"+str(assetFile.objects[k])+"\n")
 
 end = time.perf_counter()
-print(f"Extracted {len(objects)} objects in {end-start}s")
+print(f"Extracted {len(assetFile.objects)} objects in {end-start}s")
--- unityfile.py
+++ unityfile.py
@@ -8,7 +8,7 @@ class UnityObject:
     def parse(block:str, lines:tuple[int,int]) -> "UnityObject":
         header = block[0:block.find("\n")]
         matches = re.match(r"--- !u!(\d+) &(-?\d+)", header)
-        assert matches != None
+        assert matches != None, f"Expected a header, but got {header}"
         return UnityObject(
             int(matches.group(2)),
             int(matches.group(1)),
@@ -27,3 +27,30 @@ class UnityObject:
     
     def __str__(this) -> str:
         return f"--- !u!{this.__nativeType} &{this.__localid}\n{this.content}"
+    
+class UnityFile:
+    @staticmethod
+    def fromPath(path: str):
+        # Read data blocks
+        with open(path, "r") as sourceFile:
+            lines = sourceFile.readlines()
+            lines = [(i[:-1] if i.endswith("\n") else i) for i in lines] # Remove trailing newlines
+        return UnityFile(lines)
+
+    @staticmethod
+    def fromText(content: str):
+        return UnityFile(content.split("\n"))
+
+    def __init__(this, lines):
+        this.objects:dict[localid_t, UnityObject] = dict()
+        this.header:str = None
+        
+        # Split into objects
+        indices = [i for i in range(len(lines)) if lines[i].startswith("--- !u!")] + [len(lines)]
+        this.header = "\n".join(lines[0:indices[0]])
+        for i in range(len(indices)-1):
+            objContent = "\n".join(lines[ indices[i] : indices[i+1] ])
+            obj = UnityObject.parse(objContent, lines)
+            this.objects[obj.localid] = obj
+    
+        assert len(this.objects) > 0, "File was empty!"
